0b1f861ab1a195eaa273dfb8cd73fe56d8385927
Partha Das <pd6434938@gmail.com>
2026-04-25 23:50:14 +0530

    Remove unnecessary blank lines in text utility.py

--- text utility.py
+++ text utility.py
@@ -11,9 +11,6 @@ def reverse_text(text):
 print("1.Word Count 2.Vowel Count 3.Reverse")
 choice = int(input("Choose option: "))
 text = input("Enter text: ")
-
-
-
 if choice == 1:
     print("Words:", word_count(text))
 elif choice == 2:
