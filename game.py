e78a95d2e72e3ec126a22474c6444f79fe1c5fd9
Partha Das <pd6434938@gmail.com>
2026-09-12 23:54:31 +0530

    Fix input prompt formatting in game.py

--- game.py
+++ game.py
@@ -6,7 +6,7 @@ def number_guessing_game():
     attempts = 0
 
     while True:
-        guess = int(input("Guess a number between 1 and 100: "))
+        guess = int(input("Guess a number between 1 and 100: ")) 
         attempts += 1
 
         
