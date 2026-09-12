316799ef3e362d8d38eca76f0e2605a8fe76b6e1
Partha Das <pd6434938@gmail.com>
2026-09-12 23:54:48 +0530

    Fix increment of attempts in guessing game

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ def number_guessing_game():
 
     while True:
         guess = int(input("Guess a number between 1 and 100: ")) 
-        attempts += 1
+        attempts += 1 
 
         
         if guess < number_to_guess:
